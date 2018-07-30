ef0df64249a72ea8216032a74888e5a12e9d4a98
PBOCER <przemek.bocer@gmail.com>
2018-07-31 00:22:06 +0200

    Login controller

---
+++ web/src/main/java/pl/jstk/controller/LoginController.java
@@ -0,0 +1,23 @@
+package pl.jstk.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import pl.jstk.constants.ViewNames;
+
+
+@Controller
+public class LoginController {
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login() {
+        return "login";
+    }
+
+
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public String logout(Model model) {
+        return ViewNames.LOGIN;
+    }
+}
